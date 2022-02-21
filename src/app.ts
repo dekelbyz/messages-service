af02c93e708b74adec7e8fd52dd69383cf18b24e
dekelbyz <bayazi455@gmail.com>
2022-02-21 13:33:29 +0200

    add a logging middleware with the following data: method, endpoint, execution time

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
+import { date } from "joi";
 import { applicationRoutes } from "./entities/routes";
 
 const app = express();
 
 /* middleware functions. */
 app.use(express.json());
-app.use(applicationRoutes);
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  console.log(`${req.method} ${req.originalUrl} ${Date.now() - start}ms`);
+});
 
 app.get("/is-alive", (req: Request, res: Response) => {
   res.send("I am alive");
 });
+
+app.use(applicationRoutes);
 
 const PORT: number | string = process.env.PORT || 3000;
 
