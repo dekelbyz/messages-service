18b4ad09e0831db7c62cb00d340aa2d7ab9c7b36
dekelbyz <bayazi455@gmail.com>
2022-02-26 15:54:54 +0200

    Add valid test to send message method

--- tests/unit/messages/messages.controller.test.ts
+++ tests/unit/messages/messages.controller.test.ts
@@ -1,12 +1,29 @@
-import { sendMessage } from "../../../src/entities/messages/messages.controller";
-import type { SendMessageDto } from "../../../src/entities/messages/dto/send-message.dto";
+import * as controller from "../../../src/entities/messages/messages.controller";
+import { SendMessageDto } from "../../../src/entities/messages/dto/send-message.dto";
+import { dbConnect } from "../../../src/database";
 
-const messageRequest: SendMessageDto = {
-  recipient: "NeriyaCo",
-  content: "Hey. Just running some tests.",
+const messageBody: SendMessageDto = {
+  recipient: "Testing",
+  content: "Dev test",
 };
 
-test("this is a test", async () => {
-  const result = await sendMessage(messageRequest, "Dekel12345");
-  expect(result.sender).toEqual("Dekel12345");
+describe("Send Message", () => {
+  (function () {
+    dbConnect();
+  })();
+  test("Valid Send Message Request", async () => {
+    const request = await controller.sendMessage(messageBody, "Dekel");
+
+    const expectedResult = {
+      sender: "Dekel",
+      recipient: "Testing",
+      content: "Dev test",
+    };
+    const departedData = {
+      sender: request.sender,
+      recipient: request.recipient,
+      content: request.content,
+    };
+    expect(expectedResult).toMatchObject(departedData);
+  });
 });
